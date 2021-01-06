05ffd242dd6d21282192db552b22c5efea52e098
Anni Mari Prees <anpree@ttu.ee>
2021-01-06 16:43:32 +0200

    Data testide põhjad

---
+++ Tests/Data/Common/NamedEntityDataTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportEvents.Data.Common;
+
+namespace Tests.Data.Common
+{
+    [TestClass]
+    public class NamedEntityDataTests
+    {
+        private class TestClass : NamedEntityData { }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+        }
+
+        [TestMethod]
+        public void IdTest()
+        {
+
+        }
+    }
+}
