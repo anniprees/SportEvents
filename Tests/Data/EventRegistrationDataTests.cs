c33023a20fa964551335ec69dd0f39adca65ad26
Lii Saluvere <lii.saluvere@ttu.ee>
2021-01-07 17:45:29 +0200

    Data testide täiendamine

--- Tests/Data/EventRegistrationDataTests.cs
+++ Tests/Data/EventRegistrationDataTests.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportEvents.Data;
+using SportEvents.Data.Common;
 
 namespace Tests.Data
 {
     [TestClass]
-    public class EventRegistrationDataTests
+    public class EventRegistrationDataTests: SealedClassTests<EventRegistrationData, UniqueEntityData>
     {
+        [TestMethod]
+        public void EventIdTest() => IsNullableProperty(() => obj.EventId, x => obj.EventId = x);
+        [TestMethod]
+        public void ParticipantIdTest() => IsNullableProperty(() => obj.ParticipantId, x => obj.ParticipantId = x);
     }
 }
+
+
