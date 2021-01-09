63e57de4b1fee8999ec4858474767cf814ed4898
Allan Päll <alpall@ttu.ee>
2021-01-09 23:56:45 +0200

    Dummy data laadimine

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SportEvents.Infra;
 
 namespace SportEvents.WebApplication
 {
@@ -7,7 +9,14 @@ namespace SportEvents.WebApplication
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var db = services.GetRequiredService<EventsDbContext>();
+                EventsDataInitializer.Initialize(db);
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
